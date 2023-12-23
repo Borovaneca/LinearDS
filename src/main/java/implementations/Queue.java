a6058be242dde6fdf30d6f3ed280c294a842461c
Borovaneca <petyoveselinov@gmail.com>
2023-12-23 15:16:57 +0200

    Queue implementation.

--- src/main/java/implementations/Queue.java
+++ src/main/java/implementations/Queue.java
@@ -5,34 +5,82 @@ import interfaces.AbstractQueue;
 import java.util.Iterator;
 
 public class Queue<E> implements AbstractQueue<E> {
+    private Node<E> head;
+    private int size;
 
+    private static class Node<E> {
+        private E value;
+        private Node<E> next;
+
+        public Node(E element) {
+            this.value = element;
+        }
+    }
+
+    public Queue() {
+        this.head = null;
+        this.size = 0;
+    }
     @Override
     public void offer(E element) {
-
+        Node<E> toInsert = new Node<>(element);
+        if (this.isEmpty()) {
+            this.head = toInsert;
+        } else {
+            Node<E> current = this.head;
+            while (current.next != null) {
+                current = current.next;
+            }
+            current.next = toInsert;
+        }
+        this.size++;
     }
 
     @Override
     public E poll() {
-        return null;
+        if (isEmpty()) {
+            throw new IllegalStateException();
+        }
+        Node<E> first = this.head;
+        this.head = first.next;
+        this.size--;
+        return first.value;
     }
 
     @Override
     public E peek() {
-        return null;
+        if (isEmpty()) {
+            throw new IllegalStateException();
+        }
+
+        return this.head.value;
     }
 
     @Override
     public int size() {
-        return 0;
+        return this.size;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return size() == 0;
     }
 
     @Override
     public Iterator<E> iterator() {
-        return null;
+        return new Iterator<E>() {
+            private Queue.Node<E> current;
+            @Override
+            public boolean hasNext() {
+                return current != null;
+            }
+
+            @Override
+            public E next() {
+                E value = current.value;
+                current = current.next;
+                return value;
+            }
+        };
     }
 }
